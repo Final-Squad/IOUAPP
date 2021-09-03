74ce5aea13075f8a7a7080cfe168fbb4483707b4
chriswill88 <williechri79@gmail.com>
2021-09-03 10:34:00 -0400

    [view record page in progress]

--- components/ViewRecord/ViewRecord.js
+++ components/ViewRecord/ViewRecord.js
@@ -1,12 +1,41 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView, Share } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 
 const mock = require('../../assets/MOCK_DATA.json')
 
-for (const i of mock) {
-  console.log(i)
+function getObj(id) {
+  return mock.filter((i) => {
+    return i.id===id
+  })
 }
+
+function infoView(id, setinfoComp) {
+  const obj = getObj(id)[0]
+  console.log('obj ->', obj)
+
+  return (
+  <View style={{backgroundColor: 'rgba(0,0,0,0.5)', position: 'absolute', width: '100%', height: '120%', bottom: 0}}>
+    <View style={{backgroundColor: 'black', width: '90%', height: '80%', left: '5%', top: '13%' , position: 'absolute', padding: 50}}>
+      <Text style={{textAlign: 'center', color: 'white'}}>{`Name: ${obj.name}`}</Text>
+      <Text style={{textAlign: 'center', color: 'white'}}>{`Awaited Item: ${obj.thing}`}</Text>
+      <Text style={{textAlign: 'center', color: 'white'}}>{`Waiting Since: ${obj.startDate}`}</Text>
+      {obj.paid ? <Text style={{textAlign: 'center', color: 'white'}}>{`Paid on: ${obj.endDate}`}</Text> : null}
+      {!obj.paid && <Button style={{textAlign: 'center', color: 'white'}} title='Mark as Paid?'/>}
+      <Button style={{textAlign: 'center', color: 'white'}} onPress={() => setinfoComp(false)} title='Done'/>
+      {!obj.paid && <Button
+      title={'Send them a reminder'}
+      onPress={async () => {
+        await Share.share({
+          message: obj.youOwe ? `Hey ${obj.name} i havent forgotten about the ${obj.thing}` :  `Hey ${obj.name}, dont forget to give me the ${obj.thing}`
+        })
+      }}
+      />}
+    </View>
+  </View>
+  )
+}
+
 
 export default function ViewRecord({setApp}) {
   const [infoComp, setinfoComp] = useState(false)
@@ -14,39 +43,19 @@ export default function ViewRecord({setApp}) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{'Records\n'}</Text>
-
-      {/* <ScrollView
-      showsVerticalScrollIndicator={false}
-      >
-
-        {
-          data.map((rec) => {
-            return rec.paid ?
-              rec.youOwe === 'me' ?
-                <Text key={rec.id}>{`${rec.id + 1}: I paid ${rec.name} ${rec.thing}\n`}</Text>
-              :
-                <Text key={rec.id}>{`${rec.id + 1}: ${rec.name} paid me ${rec.thing}\n`}</Text>
-            :  
-            rec.youOwe == 'me' ?
-              <Text key={rec.id}>{`${rec.id + 1}: I owe ${rec.name} ${rec.thing} because ${rec.reason}\n`}</Text>
-            :
-              <Text key={rec.id}>{`${rec.id + 1}: ${rec.name} owes me ${rec.thing} because ${rec.reason}\n`}</Text>
-          })
-        }
-
-      </ScrollView> */}
       <Text style={styles.header}>Dues To Be Paid</Text>
       <ScrollView
       showsVerticalScrollIndicator={false}
+      style={{paddingHorizontal: 20}}
       >
 
         {
           mock.map((rec) => {
             return !rec.paid ?
             rec.youOwe == true ?
-              <Text onPress={() => {setinfoComp(true); setid(rec.id)}} style={{textAlign: 'center'}}key={rec.id}>{`I owe ${rec.name} ${rec.thing} because ${rec.reason}\n`}</Text>
+              <Text onPress={() => {setinfoComp(true); setid(rec.id)}} style={{}}key={rec.id}>{`I owe ${rec.name} ${rec.thing} because ${rec.reason}\n`}</Text>
               :
-              <Text onPress={() => {setinfoComp(true); setid(rec.id)}} style={{textAlign: 'center'}}key={rec.id}>{`${rec.name} owes me ${rec.thing} because ${rec.reason}\n`}</Text>
+              <Text onPress={() => {setinfoComp(true); setid(rec.id)}} style={{}}key={rec.id}>{`${rec.name} owes me ${rec.thing} because ${rec.reason}\n`}</Text>
             :
             null
           })
@@ -56,15 +65,17 @@ export default function ViewRecord({setApp}) {
       <Text style={styles.header}>Paid Dues</Text>
       <ScrollView
       showsVerticalScrollIndicator={false}
+      style={{paddingHorizontal: 20}}
+
       >
 
         {
           mock.map((rec) => {
             return rec.paid ?
               rec.youOwe === true ?
-                <Text onPress={() => {setinfoComp(true); setid(rec.id)}} style={{textAlign: 'center'}}key={rec.id}>{`I paid ${rec.name} ${rec.thing}\n`}</Text>
+                <Text onPress={() => {setinfoComp(true); setid(rec.id)}} style={{}}key={rec.id}>{`I paid ${rec.name} ${rec.thing}\n`}</Text>
                 :
-                <Text onPress={() => {setinfoComp(true); setid(rec.id)}} style={{textAlign: 'center'}}key={rec.id}>{`${rec.name} paid me ${rec.thing}\n`}</Text>
+                <Text onPress={() => {setinfoComp(true); setid(rec.id)}} style={{}}key={rec.id}>{`${rec.name} paid me ${rec.thing}\n`}</Text>
               :
               null
           })
@@ -80,13 +91,7 @@ export default function ViewRecord({setApp}) {
           }
       />
       {
-        infoComp ?
-        <View style={{backgroundColor: 'rgba(0,0,0,0.5)', position: 'absolute', width: '100%', height: '120%', bottom: 0}}>
-          <View style={{backgroundColor: 'black', width: '90%', height: '80%', left: '5%', top: '13%' , position: 'absolute', padding: 50}}>
-            <Text style={{textAlign: 'center', color: 'white'}} onPress={() => setinfoComp(false)}>Done</Text>
-          </View>
-        </View> :
-        null
+        infoComp && infoView(id, setinfoComp)
       }
     </View>
   );
